39e1b5b93efe46ca75f4a95867a38776b54ea140
Poran Hazi <poran@MacBook-Air-de-Poran.local>
2026-03-09 10:42:03 +0100

    Añadir ejercicio 4

---
+++ Ejercicio4/ejercicio4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+double capitalAcumulat(double C, int A, double R) {
+    double resultat;
+
+    resultat = C * pow((1 + R / 100.0), A);
+
+    return resultat;
+}
+
+int main() {
+    double capital, interes, final;
+    int anys;
+
+    printf("Introduce el capital inicial: ");
+    scanf("%lf", &capital);
+
+    printf("Introduce el numero de anos: ");
+    scanf("%d", &anys);
+
+    printf("Introduce el interes anual (%%): ");
+    scanf("%lf", &interes);
+
+    final = capitalAcumulat(capital, anys, interes);
+
+    printf("Capital final acumulado: %.2lf\n", final);
+
+    return 0;
+}
